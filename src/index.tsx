037ae201b0c8e1348aaf733b4699303726bda83e
Abbas Jafari <abbas.jafari.4001@gmail.com>
2022-11-20 15:57:18 +0330

    pwa update

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { AppContext } from "./_context/AppContext";
 import QueryProvider from "./_queries/QueryProvider";
 import RoutesApp from "./_routes/RoutesApp";
 import "./_styles/index.css";
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.createRoot(document.querySelector("#root") as HTMLElement).render(
     <React.StrictMode>
@@ -16,4 +16,4 @@ ReactDOM.createRoot(document.querySelector("#root") as HTMLElement).render(
     </React.StrictMode>,
 );
 
-// serviceWorkerRegistration.register();
+serviceWorkerRegistration.register();
